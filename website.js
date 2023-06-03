4e67bbad5d2033d475ed173a1d23e52a9b7f9e17
tugass <135437185+tugass@users.noreply.github.com>
2023-06-03 20:25:04 +0700

    Add files via upload

---
+++ website.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {
+
+var mybutton = document.getElementById("myBtn");
+
+
+    if (document.documentElement.scrollTop > 200) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
